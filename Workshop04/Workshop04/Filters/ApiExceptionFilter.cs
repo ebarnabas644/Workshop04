ad2874058849c5fab31d0777219b7ba069c5817d
Básthy Ádám László <basthyadi@gmail.com>
2022-10-19 11:39:31 +0200

    Add ExceptionFilter

---
+++ Workshop04/Workshop04/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Workshop04Models;
+
+namespace Workshop04.Filters
+{
+    public class ApiExceptionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception is ArgumentException ex)
+            {
+                context.ExceptionHandled = true;
+
+                context.Result = new BadRequestObjectResult(new ErrorModel()
+                {
+                    Message = ex.Message,
+                    Date = DateTime.Now
+                });
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+    }
+}
