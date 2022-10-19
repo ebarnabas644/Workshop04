31c810a412c5dcd02575b004cb9402e662c7ddce
Básthy Ádám László <basthyadi@gmail.com>
2022-10-19 11:37:52 +0200

    Add ErrorModel

---
+++ Workshop04/Workshop04Models/ErrorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop04Models
+{
+    public class ErrorModel
+    {
+        public string Message { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
